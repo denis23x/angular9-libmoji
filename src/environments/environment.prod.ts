b7f154232cfd8e65e5d3f9b9d57f1fa904954486
denis23x <damage.23x@gmail.com>
2020-10-23 20:31:55 +0600

    update env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: true,
-  api_url: 'http://localhost:8031/api',
-  avatar_url: 'http://localhost:8031/avatars'
+  // api_url: 'http://localhost:8031/api',
+  // avatar_url: 'http://localhost:8031/avatars',
+  api_url: 'http://91.210.169.73:8031/api',
+  avatar_url: 'http://91.210.169.73:8031/avatars'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,10 @@
 
 export const environment = {
   production: false,
-  api_url: 'http://localhost:8031/api',
-  avatar_url: 'http://localhost:8031/avatars'
+  // api_url: 'http://localhost:8031/api',
+  // avatar_url: 'http://localhost:8031/avatars',
+  api_url: 'http://91.210.169.73:8031/api',
+  avatar_url: 'http://91.210.169.73:8031/avatars'
 };
 
 /*
